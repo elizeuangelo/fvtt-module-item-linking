2428b782ac35cf968f0c4fd337c594fff05f968b
elizeuangelo <elizeu90@gmail.com>
2023-05-05 16:07:45 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Foundry VTT - Item-Linking
+
+![GitHub release (latest SemVer)](https://img.shields.io/github/v/release/elizeuangelo/fvtt-module-item-linking)
+![GitHub Releases](https://img.shields.io/github/downloads/elizeuangelo/fvtt-module-item-linking/latest/item-linking.zip)
+![GitHub All Releases](https://img.shields.io/github/downloads/elizeuangelo/fvtt-module-item-linking/item-linking.zip?label=downloads)
+
+Description here.
+
+## Installation
+
+In the setup screen, use the manifest URL https://raw.githubusercontent.com/elizeuangelo/fvtt-module-item-linking/master/module.json to install the module.
+
+## How to Use
+
+![Presentation](https://raw.githubusercontent.com/elizeuangelo/fvtt-module-item-linking/master/assets/presentation.gif)
+
+How to use here.
---
+++ module/settings.ts
@@ -0,0 +1,57 @@
+export const SYSTEM_ID = 'item-linking';
+
+const settings = {
+	//snapTokens: {
+	//	name: 'Snap Tokens',
+	//	scope: 'world',
+	//	config: false,
+	//	type: Boolean,
+	//	default: true,
+	//	onChange: (value) => canvas.tokens!.placeables.forEach((t) => t.refresh()),
+	//},
+	//scatter: {
+	//	name: 'Scattering',
+	//	hint: 'How much the tokens will scatter around?',
+	//	scope: 'world',
+	//	config: true,
+	//	type: Number,
+	//	default: 0.4,
+	//	range: {
+	//		min: 0.01,
+	//		max: 1,
+	//		step: 0.01,
+	//	},
+	//},
+	//ignoreDead: {
+	//	name: 'Ignore Special Cases',
+	//	hint: 'Dead or incapacitated tokens will not be snapped.',
+	//	scope: 'world',
+	//	config: true,
+	//	type: Boolean,
+	//	default: true,
+	//},
+	//sizeOrder: {
+	//	name: 'Smaller Tokens in Front',
+	//	hint: 'Takes smaller tokens in front of bigger ones, for better management in the grid.',
+	//	scope: 'world',
+	//	config: true,
+	//	type: Boolean,
+	//	default: true,
+	//},
+} as const;
+
+export type Settings = typeof settings;
+
+export function getSetting<T extends keyof Settings>(name: T) {
+	return game.settings.get(SYSTEM_ID, name) as unknown as ReturnType<Settings[T]['type']>;
+}
+
+export function setSetting<T extends keyof Settings>(name: T, value: ReturnType<Settings[T]['type']>) {
+	return game.settings.set(SYSTEM_ID, name, value);
+}
+
+Hooks.once('setup', () => {
+	for (const [key, setting] of Object.entries(settings)) {
+		game.settings.register(SYSTEM_ID, key, setting as unknown as any);
+	}
+});
