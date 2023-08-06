e2e15e3c45139e57da47811caef82ad5330cdb1f
elizeuangelo <elizeu90@gmail.com>
2023-08-06 12:09:43 -0300

    fixes actor duplication bug

--- module/actor.js
+++ module/actor.js
@@ -4,18 +4,20 @@ function preCreateActor(actor, data, options) {
     if (!options.fromCompendium || options.linkedUpdate)
         return;
     const linked = actor.items.contents.filter((i) => getFlag(i, 'isLinked') && getFlag(i, 'baseItem'));
-    Promise.all(linked.map((i) => fromUuid(getFlag(i, 'baseItem')))).then((items) => {
-        items.forEach((baseItem, idx) => {
-            if (baseItem === null)
-                return;
-            const item = linked[idx];
-            const dataSource = data.items.find((i) => i._id === item.id);
-            const changes = createChanges(item, baseItem);
-            mergeObject(dataSource, changes);
+    if (linked.length) {
+        Promise.all(linked.map((i) => fromUuid(getFlag(i, 'baseItem')))).then((items) => {
+            items.forEach((baseItem, idx) => {
+                if (baseItem === null)
+                    return;
+                const item = linked[idx];
+                const dataSource = data.items.find((i) => i._id === item.id);
+                const changes = createChanges(item, baseItem);
+                mergeObject(dataSource, changes);
+            });
+            delete data._id;
+            Actor.create(data, { ...options, linkedUpdate: true });
         });
-        delete data._id;
-        Actor.create(data, { ...options, linkedUpdate: true });
-    });
+    }
     return !linked.length;
 }
 Hooks.on('preCreateActor', preCreateActor);
--- module/actor.ts
+++ module/actor.ts
@@ -12,17 +12,22 @@ interface Options {
 function preCreateActor(actor: Actor, data, options: Options) {
 	if (!options.fromCompendium || options.linkedUpdate) return;
 	const linked = actor.items.contents.filter((i) => getFlag(i, 'isLinked') && getFlag(i, 'baseItem'));
-	Promise.all(linked.map((i) => fromUuid(getFlag(i, 'baseItem')!))).then((items: (Item | null)[]) => {
-		items.forEach((baseItem: ItemExtended | null, idx) => {
-			if (baseItem === null) return;
-			const item = linked[idx] as ItemExtended;
-			const dataSource = data.items.find((i) => i._id === item.id);
-			const changes = createChanges(item, baseItem);
-			mergeObject(dataSource, changes);
+
+	if (linked.length) {
+		Promise.all(linked.map((i) => fromUuid(getFlag(i, 'baseItem')!))).then((items: (Item | null)[]) => {
+			items.forEach((baseItem: ItemExtended | null, idx) => {
+				if (baseItem === null) return;
+				const item = linked[idx] as ItemExtended;
+				const dataSource = data.items.find((i) => i._id === item.id);
+				const changes = createChanges(item, baseItem);
+				mergeObject(dataSource, changes);
+			});
+
+			delete data._id;
+			Actor.create(data, { ...options, linkedUpdate: true });
 		});
-		delete data._id;
-		Actor.create(data, { ...options, linkedUpdate: true });
-	});
+	}
+
 	return !linked.length;
 }
 
